dff3077bb742cd2982b33d0c3e49439a9c7576b6
unknown <frieske.jakub@gmail.com>
2020-10-27 08:40:12 +0100

    Add method

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -2,4 +2,7 @@ public class Hello {
   public static void main(String[] args){
     System.out.print("Hello World");
   }
+  public void metoda1(){
+    System.out.println("Ta metoda nic nie zwraca");
+  }
 }
